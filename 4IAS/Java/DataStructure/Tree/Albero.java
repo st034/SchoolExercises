f0ec91734dbd1455835f00d303acb161db9d3b34
Giodots <dodedodo96@gmail.com>
2019-04-18 20:10:48 +0200

    added depth, exists and nOfElements methods

--- 4IAS/Java/DataStructure/Tree/Albero.java
+++ 4IAS/Java/DataStructure/Tree/Albero.java
@@ -1,7 +1,9 @@
 public class Albero{
 	Nodo root;
+	int count;
 	public Albero(){
 		root=null;
+		count=0;
 	}
 	public void insertElement(int info){
 		if(root==null){
@@ -10,49 +12,49 @@ public class Albero{
 			insertElement(info, root);
 		}
 	}
-	private void insertElement(int info, Nodo tmp){
-		if(info<tmp.info){
-			if(tmp.sx==null){
-				tmp.sx=new Nodo(info, null, null);
+	private void insertElement(int info, Nodo root){
+		if(info<root.info){
+			if(root.sx==null){
+				root.sx=new Nodo(info, null, null);
 			}else{
-				insertElement(info, tmp.sx);
+				insertElement(info, root.sx);
 			}
 		}else{
-			if(tmp.dx==null){
-				tmp.dx=new Nodo(info, null, null);
+			if(root.dx==null){
+				root.dx=new Nodo(info, null, null);
 			}else{
-				insertElement(info, tmp.dx);
+				insertElement(info, root.dx);
 			}
 		}
 	}
 	public void inOrder(){
 		inOrder(root);
 	}
-	private void inOrder(Nodo tmp){
-		if(tmp!=null){
-			inOrder(tmp.sx);
-			System.out.println(tmp.info);
-			inOrder(tmp.dx);
+	private void inOrder(Nodo root){
+		if(root!=null){
+			inOrder(root.sx);
+			count++;
+			inOrder(root.dx);
 		}
 	}
 	public void postOrder(){
 		postOrder(root);
 	}
-	private void postOrder(Nodo tmp){
-		if(tmp!=null){
-			postOrder(tmp.sx);
-			postOrder(tmp.dx);
-			System.out.println(tmp.info);
+	private void postOrder(Nodo root){
+		if(root!=null){
+			postOrder(root.sx);
+			postOrder(root.dx);
+			System.out.println(root.info);
 		}
 	}
 	public void preOrder(){
 		preOrder(root);
 	}
-	private void preOrder(Nodo tmp){
-		if(tmp!=null){
-			System.out.println(tmp.info);
-			preOrder(tmp.sx);
-			preOrder(tmp.dx);
+	private void preOrder(Nodo root){
+		if(root!=null){
+			System.out.println(root.info);
+			preOrder(root.sx);
+			preOrder(root.dx);
 		}
 	}
 	public boolean exists(int el){
@@ -75,5 +77,46 @@ public class Albero{
 	}
 	public boolean isEmpty(){
 		return root==null;
+	}
+	public void resetCount(){
+		count=0;
+	}
+	public void clearTree(){
+		root=null;
+	}
+	public int depth(){
+		if(root==null){
+			return 0;
+		}else{
+			return depth(root);
+		}
+	}
+	private int depth(Nodo root){
+		if(root==null){
+			return 0;
+		}else{
+			return 1+getGreater(depth(root.sx),depth(root.dx));
+		}		
+	}
+	private int getGreater(int val1, int val2){
+		if(val1>val2){
+			return val1;
+		}else{
+			return val2;
+		}
+	}
+	public int noOfElements(){
+		if(root==null){
+			return 0;
+		}else{
+			return noOfElements(root,1);
+		}
+	}
+	private int noOfElements(Nodo root, int count){
+		if(root==null){
+			return 0;
+		}else{
+			return 1+noOfElements(root.sx, 1+noOfElements(root.dx, count));
+		}
 	}
 }
