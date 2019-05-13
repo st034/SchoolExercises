cc6ab948d7581c1a532226482c719fb964a1acfc
Giodots <dodedodo96@gmail.com>
2019-05-13 21:29:42 +0200

    Implemented iterator for BST DS for use For Each

--- 4IAS/Java/Iteratori/Tree/AlberoIterator.java
+++ 4IAS/Java/Iteratori/Tree/AlberoIterator.java
@@ -1,15 +1,23 @@
 import java.util.Iterator;
 public class AlberoIterator implements Iterator{
-	Node cursor;
-	public AlberoIterator(AlberoIterable a){
-		cursor = a.root;
+	Stack<Node> stack;
+	Node root;
+	public AlberoIterator(AlberoIterable tree){
+		root = tree.root;
+		stack = new Stack();
+		fillStack(root);
 	}
 	public boolean hasNext(){
-		return cursor != null;
+		return !stack.isEmpty();
 	}
 	public Object next(){
-		Object tmp = cursor.info;
-		cursor = cursor.sx;
-		return tmp;
+		return stack.pop().info;
+	}
+	private void fillStack(Node root){
+		if(root!=null){
+			if(root.dx!=null){fillStack(root.dx);}
+			stack.push(root);
+			if(root.sx!=null){fillStack(root.sx);}
+		}
 	}
 }
--- 4IAS/Java/Iteratori/Tree/Main.java
+++ 4IAS/Java/Iteratori/Tree/Main.java
@@ -1,18 +1,14 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		Albero a=new Albero();
+		AlberoIterable a = new AlberoIterable();
 		int[] arr={12,6,3,7,20,15,21};
+		
 		for(int i=0; i<7; i++){
-			//a.insertElement((int)(Math.random()*100));
 			a.insertElement(arr[i]);
 		}
-		//a.preOrder();
-		a.bracketsPrint();
-		System.out.println();
-		a.indentPrint();
-		a.brePreorder();
-		System.out.println();
-		a.breInorder();
+		for(Object x: a){
+			System.out.println(x);
+		}
 	}
 }
