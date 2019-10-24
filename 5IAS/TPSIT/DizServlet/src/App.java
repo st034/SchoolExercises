33c4c7f903fbb726e2ee54f727efe8f437cb8aa6
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-10-25 00:16:56 +0200

    now you can add new words

--- 5IAS/TPSIT/DizServlet/src/App.java
+++ 5IAS/TPSIT/DizServlet/src/App.java
@@ -1,5 +1,5 @@
 import java.io.*;
-
+import java.nio.file.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +11,7 @@ public class App extends HttpServlet{
     
     private static final long serialVersionUID = -3967314453512919811L;
     private JSONObject jsonObject;
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java 1");
     }
@@ -18,7 +19,7 @@ public class App extends HttpServlet{
     public void init() throws ServletException {
         try{
             JSONParser parser = new JSONParser();
-            FileReader reader = new FileReader("C:/Users/GioDots/Code/dodesini/5IAS/TPSIT/ServletExercise1/dictionary.json");
+            FileReader reader = new FileReader("C:/Users/GioDots/Code/dodesini/5IAS/TPSIT/DizServlet/dictionary.json");
             jsonObject = (JSONObject) parser.parse(reader);
         }catch(IOException e){
             throw new RuntimeException(e);
@@ -34,15 +35,42 @@ public class App extends HttpServlet{
         PrintWriter out = res.getWriter();
         out.println("<h1>Hi</h1>");      
         out.println("<p>Search a word:</p>");
-        out.println("<form action='http://localhost:8080/WebContent/welcome' method='post'><input type='text' name='word'></input><input type='submit'><form>");
-        
+        out.println("<form action='http://localhost:8080/WebContent/welcome' method='post'>"+
+                    "<input type='text' name='word'><br>"+
+                    "<input type='submit'>"+
+                    "</form>");
+        out.println("<p>Add word:</p>"+
+                    "<form action='http://localhost:8080/WebContent/welcome' method='post'>"+
+                    "Parola: <input type='text' name='addword'><br>"+
+                    "Significato: <input type='text' name='meaning'><br>"+
+                    "<input type='submit'>"+
+                    "</form>");
     }
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
         res.setContentType("text/html");
         String word = req.getParameter("word");
+        if(word == null){
+            addWord(req, res);
+        }else{
+            returnMeaning(req,res, word);
+            
+        }
+    }
+    public void returnMeaning(HttpServletRequest req, HttpServletResponse res, String word) throws IOException{
         PrintWriter out = res.getWriter();
         String result = (String) jsonObject.get(word.toLowerCase());
         out.println("<h1>Meaning of "+word+":</h1>");
         out.println("<p>"+result+"</p");
+    }
+    public void addWord(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        res.setContentType("text/html");
+        String filename= "C:/Users/GioDots/Code/dodesini/5IAS/TPSIT/DizServlet/dictionary.json";
+        String word = req.getParameter("addword");
+        String meaning = req.getParameter("meaning");
+        jsonObject.put(word.toLowerCase(), meaning);
+        Files.write(Paths.get(filename), jsonObject.toJSONString().getBytes());
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Added:</h2>"+
+                    "<ul><li>"+word+": "+meaning+"</li></ul>");
     }
 }
