df56a135581c238d44c909fd76e83bac1a01367d
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-11-01 11:29:06 +0100

    it works

--- 5IAS/TPSIT/DizServletJSON/src/App.java
+++ 5IAS/TPSIT/DizServletJSON/src/App.java
@@ -3,7 +3,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
