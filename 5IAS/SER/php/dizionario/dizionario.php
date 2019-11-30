72744d7d80f79148dcb1e5acfdd09995005a8dc7
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-11-30 16:56:00 +0100

    content align center

--- 5IAS/SER/php/dizionario/dizionario.php
+++ 5IAS/SER/php/dizionario/dizionario.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height: 100%;">
 
 <head>
     <meta charset="UTF-8">
@@ -10,69 +10,73 @@
     <title>Dizionario</title>
 </head>
 
-<body>
-    <div class="container">
-        <div class="jumbotron shadow p-3 mb-5 rounded">
-            <?php 
-                error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING );
-                $fileName = "dizionario.json";
-                $option = $_POST["option"];
-                $word = lcfirst($_POST["word"]);
-                
-                
-                $dictionary = file_get_contents($fileName);
-                $dictionary = json_decode($dictionary, true);
-                if($option == "get") {
-                    $meaning = $dictionary[$word];
-                    if(!isset($meaning)){
-                        $meaning="sorry, word not found";
-                    }
-                    $meaning = $meaning;
-                    $word = $word;
-                }
-                elseif($option == "put"){
-                    $temp = $dictionary[$word];
-                    if(!isset($temp)){
-                        $tmpMeaning = $_POST["meaning"];
-                        $dictionary[$word] = $tmpMeaning;
-                        $dictionary = json_encode($dictionary);
-                        file_put_contents($fileName,"$dictionary");
-                        $meaning = "Word added";
-                    }
-                    else{
-                        $meaning = "Word already exists";
-                    }
-                }
-                elseif($option == "post"){
-                    $temp = $dictionary[$word];
-                    if(isset($temp)){
-                        $tmpMeaning = $_POST["meaning"];
-                        $dictionary[$word] = $tmpMeaning;
-                        $dictionary = json_encode($dictionary);
-                        file_put_contents($fileName,"$dictionary");
-                        $meaning = "Word modified";
-                    }
-                    else{
-                        $meaning = "Sorry, word not found";
-                    }
-                }
-                elseif($option == "delete"){
-                    $temp = $dictionary[$word];
-                    if(isset($temp)){
-                        unset($dictionary[$word]);
-                        $dictionary = json_encode($dictionary);
-                        file_put_contents($fileName,"$dictionary");
-                        $meaning = "Word deleted";
-                    }
-                    else{
-                        $meaning = "Sorry, word not found";
-                    }
-                }
-            ?>
-            <h1 class="display-4"><?php echo ucfirst($word); ?></h1>
-            <p class='lead'><?php echo ucfirst($meaning)?></p>
-            <hr class='my-4'>
-            <button type="button" class="btn btn-secondary btn-sm" onclick="document.location.href='index.html'">Return</button>
+<body style="height: 100%;">
+    <div class="container-fluid h-100">
+        <div class="row justify-content-center align-middle h-100">
+            <div class="col-10 align-self-center">
+                <div class="jumbotron shadow p-3 mb-5 rounded">
+                    <?php 
+                        error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING );
+                        $fileName = "dizionario.json";
+                        $option = $_POST["option"];
+                        $word = lcfirst($_POST["word"]);
+                        
+                        
+                        $dictionary = file_get_contents($fileName);
+                        $dictionary = json_decode($dictionary, true);
+                        if($option == "get") {
+                            $meaning = $dictionary[$word];
+                            if(!isset($meaning)){
+                                $meaning="sorry, word not found";
+                            }
+                            $meaning = $meaning;
+                            $word = $word;
+                        }
+                        elseif($option == "put"){
+                            $temp = $dictionary[$word];
+                            if(!isset($temp)){
+                                $tmpMeaning = $_POST["meaning"];
+                                $dictionary[$word] = $tmpMeaning;
+                                $dictionary = json_encode($dictionary);
+                                file_put_contents($fileName,"$dictionary");
+                                $meaning = "Word added";
+                            }
+                            else{
+                                $meaning = "Word already exists";
+                            }
+                        }
+                        elseif($option == "post"){
+                            $temp = $dictionary[$word];
+                            if(isset($temp)){
+                                $tmpMeaning = $_POST["meaning"];
+                                $dictionary[$word] = $tmpMeaning;
+                                $dictionary = json_encode($dictionary);
+                                file_put_contents($fileName,"$dictionary");
+                                $meaning = "Word modified";
+                            }
+                            else{
+                                $meaning = "Sorry, word not found";
+                            }
+                        }
+                        elseif($option == "delete"){
+                            $temp = $dictionary[$word];
+                            if(isset($temp)){
+                                unset($dictionary[$word]);
+                                $dictionary = json_encode($dictionary);
+                                file_put_contents($fileName,"$dictionary");
+                                $meaning = "Word deleted";
+                            }
+                            else{
+                                $meaning = "Sorry, word not found";
+                            }
+                        }
+                    ?>
+                    <h1 class="display-4"><?php echo ucfirst($word); ?></h1>
+                    <p class='lead'><?php echo ucfirst($meaning)?></p>
+                    <hr class='my-4'>
+                    <button type="button" class="btn btn-secondary btn-sm" onclick="document.location.href='index.html'">Return</button>
+                </div>
+            </div>
         </div>
     </div>
     
